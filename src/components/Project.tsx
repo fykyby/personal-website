78518512567bc5d197211468a23f811b097c729b
fykyby <iploritpl@gmail.com>
2022-10-12 20:03:39 +0200

    Projects section

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,47 @@
 import "../styles/Project.scss";
 
-export default function Project(): JSX.Element {
-  return <article className="Project"></article>;
+interface Props {
+  img: string;
+  title: string;
+  repoLink: string;
+  demoLink: string;
+  techIcons: Array<string>;
+}
+
+export default function Project(props: Props): JSX.Element {
+  return (
+    <article className="Project">
+      <img src={props.img} alt="project screenshot" />
+      <div className="info">
+        <h3 className="title">{props.title}</h3>
+        <div className="bottomWrapper">
+          <div className="left">
+            {props.techIcons.map((item, index) => {
+              return <i className={item} key={index}></i>;
+            })}
+          </div>
+          <div className="right">
+            <a href={props.repoLink} target="_blank" rel="noreferrer">
+              <i className="devicon-github-original"></i>
+            </a>
+            <a href={props.demoLink} target="_blank" rel="noreferrer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,7 +10,47 @@ export default function Projects(): JSX.Element {
     <section className="Projects">
       <h1>Projects</h1>
       <div className="projectList">
-        <Project />
+        <Project
+          title="Music Player for YouTube"
+          img={imgYtMusic}
+          repoLink={"https://github.com/fykyby/music-player-for-youtube"}
+          demoLink={"https://fykyby.github.io/music-player-for-youtube/"}
+          techIcons={[
+            "devicon-react-original colored",
+            "devicon-typescript-plain colored",
+          ]}
+        />
+        <Project
+          title="Fake-Instagram"
+          img={imgFakeInsta}
+          repoLink={"https://github.com/fykyby/fake-instagram"}
+          demoLink={"https://fykyby.github.io/fake-instagram/"}
+          techIcons={[
+            "devicon-react-original colored",
+            "devicon-tailwindcss-plain colored",
+            "devicon-firebase-plain colored",
+          ]}
+        />
+        <Project
+          title="Battleship"
+          img={imgBattleship}
+          repoLink={"https://github.com/fykyby/battleship"}
+          demoLink={"https://fykyby.github.io/battleship/"}
+          techIcons={[
+            "devicon-javascript-plain colored",
+            "devicon-css3-plain colored",
+          ]}
+        />
+        <Project
+          title="Snake"
+          img={imgSnake}
+          repoLink={"https://github.com/fykyby/snake"}
+          demoLink={"https://fykyby.github.io/snake/"}
+          techIcons={[
+            "devicon-javascript-plain colored",
+            "devicon-css3-plain colored",
+          ]}
+        />
       </div>
     </section>
   );
