657acfe30210972f1f767b43b0ced83758ca17e6
fykyby <iploritpl@gmail.com>
2022-10-13 20:06:34 +0200

    Lots of things

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,33 @@
 import "../styles/About.scss";
+const scrollToElement = require("scroll-to-element");
 
 export default function About(): JSX.Element {
+  function scrollToSection(
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    targetSelector: string
+  ) {
+    e.preventDefault();
+    scrollToElement(targetSelector, {
+      ease: "out-sine",
+      duration: 600,
+    });
+  }
+
   return (
     <section className="About">
-      <a href="https://github.com/fykyby" target="_blank" rel="noreferrer">
-        GitHub
-      </a>
+      <nav>
+        <a href="https://github.com/fykyby" target="_blank" rel="noreferrer">
+          GitHub
+        </a>
+        <a
+          href="/"
+          onClick={(e) => {
+            scrollToSection(e, ".Contact");
+          }}
+        >
+          Contact
+        </a>
+      </nav>
       <h1>Filip Bodnar</h1>
       <h2>Frontend Developer</h2>
     </section>
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,3 +1,12 @@
+import "../styles/Contact.scss";
+
 export default function Contact(): JSX.Element {
-  return <section>asd</section>;
+  return (
+    <section className="Contact">
+      <h1>Contact</h1>
+      <div>
+        <p>filipbodnar02@gmail.com</p>
+      </div>
+    </section>
+  );
 }
