ab693749b33a6fba016f58e83cc05f25005d6caa
fykyby <iploritpl@gmail.com>
2022-10-13 18:42:01 +0200

    Tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./styles/App.scss";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 export default function App(): JSX.Element {
   return (
     <main className="App">
       <About />
       <Projects />
+      <Contact />
     </main>
   );
 }
